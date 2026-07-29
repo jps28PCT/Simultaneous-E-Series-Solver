1115ccc733ae460ee6129febbfaa26fc3d6a65e9
Jacob Smithmyer <jps28@pct.edu>
2026-07-29 08:37:17 -0400

    Updated engineering notation constants in e_val_select() to be correct and unambigous.

--- e_vals.py
+++ e_vals.py
@@ -142,23 +142,23 @@ def e_val_select(components: str, relationships: list, e_series_selection: tuple
         pretty_name='\u221A3'): 1.73205080756887729352744634150587,
         
         ### ENGINEERING NOTATION
-        sp.Symbol('Y'):        10e24,
-        sp.Symbol('Z'):        10e21,
-        sp.Symbol('E'):        10e18,
-        sp.Symbol('P'):        10e15,
-        sp.Symbol('T'):        10e12,
-        sp.Symbol('G'):        10e9,
-        sp.Symbol('M'):        10e6,
-        sp.Symbol('k'):        10e3,
-        sp.Symbol('m'):        10e-3,
+        sp.Symbol('Y'):        10**24,
+        sp.Symbol('Z'):        10**21,
+        sp.Symbol('E'):        10**18,
+        sp.Symbol('P'):        10**15,
+        sp.Symbol('T'):        10**12,
+        sp.Symbol('G'):        10**9,
+        sp.Symbol('M'):        10**6,
+        sp.Symbol('k'):        10**3,
+        sp.Symbol('m'):        10**-3,
         sp.Symbol('u',
-        pretty_name='\u03BC'): 10e-6,
-        sp.Symbol('n'):        10e-9,
-        sp.Symbol('p'):        10e-12,
-        sp.Symbol('f'):        10e-15,
-        sp.Symbol('a'):        10e-18,
-        sp.Symbol('z'):        10e-21,
-        sp.Symbol('y'):        10e-24}
+        pretty_name='\u03BC'): 10**-6,
+        sp.Symbol('n'):        10**-9,
+        sp.Symbol('p'):        10**-12,
+        sp.Symbol('f'):        10**-15,
+        sp.Symbol('a'):        10**-18,
+        sp.Symbol('z'):        10**-21,
+        sp.Symbol('y'):        10**-24}
     # Concatenate dictionaries of symbols and constants for substitution
     local_dict = {s.name: s for s in syms} | {c.name: c for c in constants.keys()} 
 
