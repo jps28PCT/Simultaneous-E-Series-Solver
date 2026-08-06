6a44e6c3b715c75c634391b2aa6d7ccfdc81b706
Jacob Smithmyer <jps28@pct.edu>
2026-08-06 08:31:15 -0400

    Add unit tests for eng_note function
    Added unit tests for engineering notation handling of zero, positive infinity, and negative infinity in both ASCII and UTF-8 encodings.

--- tests/test_eng_notation.py
+++ tests/test_eng_notation.py
@@ -4,3 +4,35 @@ This test is for the engineering notation parser and engineering notation string
 
 import unittest
 from e_vals import eng_note, eng_to_float
+
+
+class test_engineering_notation(unittest.TestCase):
+  testFloat = 3.1415926535897932384
+  
+  def float_to_eng_ascii_zero(self):
+    returnedStr = eng_note(inputValue=0.0, numSigFigs=1, encoding="ASCII")
+    self.assertEqual(returnedStr, "0  ")
+
+  def float_to_eng_utf8_zero(self):
+      returnedStr = eng_note(inputValue=0.0, numSigFigs=1, encoding="UTF-8")
+      self.assertEqual(returnedStr, "0  ")
+
+  def float_to_eng_ascii_inf(self):
+      returnedStr = eng_note(inputValue=float('inf'), numSigFigs=1, encoding="ASCII")
+      self.assertEqual(returnedStr, "inf  ")
+
+  def float_to_eng_utf8_inf(self):
+      returnedStr = eng_note(inputValue=float('inf'), numSigFigs=1, encoding="UTF-8")
+      self.assertEqual(returnedStr, "\u221E  ")
+
+  def float_to_eng_ascii_neg_inf(self):
+      returnedStr = eng_note(inputValue=float('-inf'), numSigFigs=1, encoding="ASCII")
+      self.assertEqual(returnedStr, "-inf  ")
+
+  def float_to_eng_utf8_neg_inf(self):
+      returnedStr = eng_note(inputValue=float('inf'), numSigFigs=1, encoding="UTF-8")
+      self.assertEqual(returnedStr, "-\u221E  ")
+
+
+if __name__ == '__main__':
+    unittest.main()
