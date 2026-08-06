1801c5ff33d3a255bc9dd2932e9d5935cf84ff1f
Jacob Smithmyer <jps28@pct.edu>
2026-08-06 08:54:35 -0400

    Rename test methods for proper unittest parsing
    Added required "test_" prefix to test function names, and added "-inf" to UTF-8 eng_note() negative infinity test function call so it actually tested negative infinity

--- tests/test_eng_notation.py
+++ tests/test_eng_notation.py
@@ -9,28 +9,28 @@ from e_vals import eng_note, eng_to_float
 class test_engineering_notation(unittest.TestCase):
   testFloat = 3.1415926535897932384
   
-  def float_to_eng_ascii_zero(self):
+  def test_float_to_eng_ascii_zero(self):
     returnedStr = eng_note(inputValue=0.0, numSigFigs=1, encoding="ASCII")
     self.assertEqual(returnedStr, "0  ")
 
-  def float_to_eng_utf8_zero(self):
+  def test_float_to_eng_utf8_zero(self):
       returnedStr = eng_note(inputValue=0.0, numSigFigs=1, encoding="UTF-8")
       self.assertEqual(returnedStr, "0  ")
 
-  def float_to_eng_ascii_inf(self):
+  def test_float_to_eng_ascii_inf(self):
       returnedStr = eng_note(inputValue=float('inf'), numSigFigs=1, encoding="ASCII")
       self.assertEqual(returnedStr, "inf  ")
 
-  def float_to_eng_utf8_inf(self):
+  def test_float_to_eng_utf8_inf(self):
       returnedStr = eng_note(inputValue=float('inf'), numSigFigs=1, encoding="UTF-8")
       self.assertEqual(returnedStr, "\u221E  ")
 
-  def float_to_eng_ascii_neg_inf(self):
+  def test_float_to_eng_ascii_neg_inf(self):
       returnedStr = eng_note(inputValue=float('-inf'), numSigFigs=1, encoding="ASCII")
       self.assertEqual(returnedStr, "-inf  ")
 
-  def float_to_eng_utf8_neg_inf(self):
-      returnedStr = eng_note(inputValue=float('inf'), numSigFigs=1, encoding="UTF-8")
+  def test_float_to_eng_utf8_neg_inf(self):
+      returnedStr = eng_note(inputValue=float('-inf'), numSigFigs=1, encoding="UTF-8")
       self.assertEqual(returnedStr, "-\u221E  ")
 
 
