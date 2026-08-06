ead6c9d76e677471e84b4d0e9dd6c3067fa747d1
Jacob Smithmyer <jps28@pct.edu>
2026-08-06 11:34:10 -0400

    Add tests for float to engineering notation ASCII prefixes

--- tests/test_eng_notation.py
+++ tests/test_eng_notation.py
@@ -52,5 +52,66 @@ class test_engineering_notation(unittest.TestCase):
         returnedStr = eng_note(inputValue=self.TEST_FLOAT, numSigFigs=sig_fig)
         self.assertEqual(returnedStr, expected)
 
+  def test_float_to_eng_ascii_prefixes(self):
+    prefixes_cases = [
+              (self.TEST_FLOAT*10**-24, "3.1416 y"),
+              (self.TEST_FLOAT*10**-23, "31.416 y"),
+              (self.TEST_FLOAT*10**-22, "314.16 y"),
+              (self.TEST_FLOAT*10**-21, "3.1416 z"),
+              (self.TEST_FLOAT*10**-20, "31.416 z"),
+              (self.TEST_FLOAT*10**-19, "314.16 z"),
+              (self.TEST_FLOAT*10**-18, "3.1416 a"),
+              (self.TEST_FLOAT*10**-17, "31.416 a"),
+              (self.TEST_FLOAT*10**-16, "314.16 a"),
+              (self.TEST_FLOAT*10**-15, "3.1416 f"),
+              (self.TEST_FLOAT*10**-14, "31.416 f"),
+              (self.TEST_FLOAT*10**-13, "314.16 f"),
+              (self.TEST_FLOAT*10**-12, "3.1416 p"),
+              (self.TEST_FLOAT*10**-11, "31.416 p"),
+              (self.TEST_FLOAT*10**-10, "314.16 p"),
+              (self.TEST_FLOAT*10**-9,  "3.1416 n"),
+              (self.TEST_FLOAT*10**-8,  "31.416 n"),
+              (self.TEST_FLOAT*10**-7,  "314.16 n"),
+              (self.TEST_FLOAT*10**-6,  "3.1416 u"),
+              (self.TEST_FLOAT*10**-5,  "31.416 u"),
+              (self.TEST_FLOAT*10**-4,  "314.16 u"),
+              (self.TEST_FLOAT*10**-3,  "3.1416 m"),
+              (self.TEST_FLOAT*10**-2,  "31.416 m"),
+              (self.TEST_FLOAT*10**-1,  "314.16 m"),
+              (self.TEST_FLOAT*10**0,   "3.1416  "),
+              (self.TEST_FLOAT*10**1,   "31.416  "),
+              (self.TEST_FLOAT*10**2,   "314.16  "),
+              (self.TEST_FLOAT*10**3,   "3.1416 k"),
+              (self.TEST_FLOAT*10**4,   "31.416 k"),
+              (self.TEST_FLOAT*10**5,   "314.16 k"),
+              (self.TEST_FLOAT*10**6,   "3.1416 M"),
+              (self.TEST_FLOAT*10**7,   "31.416 M"),
+              (self.TEST_FLOAT*10**8,   "314.16 M"),
+              (self.TEST_FLOAT*10**9,   "3.1416 G"),
+              (self.TEST_FLOAT*10**10,  "31.416 G"),
+              (self.TEST_FLOAT*10**11,  "314.16 G"),
+              (self.TEST_FLOAT*10**12,  "3.1416 T"),
+              (self.TEST_FLOAT*10**13,  "31.416 T"),
+              (self.TEST_FLOAT*10**14,  "314.16 T"),
+              (self.TEST_FLOAT*10**15,  "3.1416 P"),
+              (self.TEST_FLOAT*10**16,  "31.416 P"),
+              (self.TEST_FLOAT*10**17,  "314.16 P"),
+              (self.TEST_FLOAT*10**18,  "3.1416 E"),
+              (self.TEST_FLOAT*10**19,  "31.416 E"),
+              (self.TEST_FLOAT*10**20,  "314.16 E"),
+              (self.TEST_FLOAT*10**21,  "3.1416 Z"),
+              (self.TEST_FLOAT*10**22,  "31.416 Z"),
+              (self.TEST_FLOAT*10**23,  "314.16 Z"),
+              (self.TEST_FLOAT*10**24,  "3.1416 Y"),
+              (self.TEST_FLOAT*10**25,  "31.416 Y"),
+              (self.TEST_FLOAT*10**26,  "314.16 Y")]
+    
+    for test_value, expected in prefixes_cases:
+      with self.subTest(test_value=test_value):
+        returnedStr = eng_note(inputValue=test_value, numSigFigs=5)
+        self.assertEqual(returnedStr, expected)
+
+
+
 if __name__ == '__main__':
     unittest.main()
