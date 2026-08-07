75ed23b74f7ab95950c1db5744b908a1bd4d4ce6
Jacob Smithmyer <jps28@pct.edu>
2026-08-07 08:32:24 -0400

    Added component first letter validation check

--- tests/test_input_checks.py
+++ tests/test_input_checks.py
@@ -3,11 +3,18 @@ This test is for the input validation functions.
 """
 
 import unittest
-from e_vals import component_check, relationship_check, e_series_selection_check, decade_check
+from e_vals import InvalidValueError, component_check, relationship_check, e_series_selection_check, decade_check
 
 
 class test_input_validation(unittest.TestCase):
 
+    def test_component_first_letter_str(self):
+        returnedStr = component_check(component="1R", out="str")
+        self.assertEqual(returnedStr, "First character is not a letter.")
+    
+    def test_component_first_letter_err(self):
+        with self.assertRaisesRegex(InvalidValueError, "First character is not a letter."):
+            component_check(component="1R", out="exception")
 
 
 if __name__ == '__main__':
