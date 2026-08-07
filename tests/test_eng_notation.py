1a627eada39bb0eb8f3eda2b919bbc8dde1aa48c
Jacob Smithmyer <jps28@pct.edu>
2026-08-07 08:04:47 -0400

    Added test for exception in eng_to_float

--- tests/test_eng_notation.py
+++ tests/test_eng_notation.py
@@ -261,7 +261,7 @@ class test_engineering_notation(unittest.TestCase):
     self.assertEqual(returnedFloat, 3.1416e-6)
 
   def test_eng_to_float_invalid_string(self):
-    with self.assertRaises(ValueError, msg="'x' is an invalid engineering notation prefix. \nPrefix must be between 10^-24 (y-) and 10^24 (Y-)."):
+    with self.assertRaisesRegex(ValueError, "invalid"):
       eng_to_float(inputStr="3.1416 x")
 
 if __name__ == '__main__':
