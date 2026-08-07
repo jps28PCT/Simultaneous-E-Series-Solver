d69545742d33935b8272d3f933844cf6fac69a78
Jacob Smithmyer <jps28@pct.edu>
2026-08-07 08:16:13 -0400

    Setup structure

--- tests/test_input_checks.py
+++ tests/test_input_checks.py
@@ -4,3 +4,11 @@ This test is for the input validation functions.
 
 import unittest
 from e_vals import component_check, relationship_check, e_series_selection_check, decade_check
+
+
+class test_input_validation(unittest.TestCase):
+
+
+
+if __name__ == '__main__':
+    unittest.main()
