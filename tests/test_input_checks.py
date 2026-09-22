015715f33e4d7f3ddce062cb6bf62f49c3156c7f
Jacob Smithmyer <jps28@pct.edu>
2026-09-22 17:26:53 -0400

    Added tests for entering zero for decade checks.

--- tests/test_input_checks.py
+++ tests/test_input_checks.py
@@ -138,7 +138,13 @@ class test_input_validation(unittest.TestCase):
             if round_to < 0:
                 round_to = 0
 
+    def test_decade_check_zero_str(self):
+        returnedStr = decade_check(decade=0, out="str")
+        self.assertEqual(returnedStr, "Decade cannot be zero.")
 
+    def test_decade_check_zero_err(self):
+        with self.assertRaisesRegex(InvalidValueError, "Decade cannot be zero."):
+            decade_check(decade=0, out="exception")
 
 
 
